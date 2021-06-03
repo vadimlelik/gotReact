52ee18e5ce6213b5645973f4401aa357c5520bb9
vadimcvirko <promiti@yandex.ru>
2021-06-03 12:58:00 +0300

    add random char

--- src/services/gotService.js
+++ src/services/gotService.js
@@ -6,10 +6,10 @@ export default class GotService {
 
     getResource = async (url) => {
         const res = await fetch(`${this._apiBase}${url}`);
-    
+
         if (!res.ok) {
-          throw new Error(`Could not fetch ${url}` +
-            `, received ${res.status}`);
+            throw new Error(`Could not fetch ${url}` +
+                `, received ${res.status}`);
         }
         return await res.json();
     }
@@ -17,24 +17,40 @@ export default class GotService {
     getAllBooks() {
         return this.getResource(`/books/`);
     }
-    
+
     getBook(id) {
         return this.getResource(`/books/${id}/`);
     }
-    
-    getAllCharacters() {
-        return this.getResource(`/characters?page=5&pageSize=10`);
+
+    async getAllCharacters() {
+        const res = await this.getResource(`/characters?page=5&pageSize=10`);
+        return res.map(this._transformCharacter)
     }
-    
-    getCharacter (id) {
-        return this.getResource(`/characters/${id}`);
+
+    async getCharacter(id) {
+       const character = await this.getResource(`/characters/${id}`);
+       return this._transformCharacter(character)
+
     }
-    
+
     getAllHouses() {
         return this.getResource(`/houses/`);
     }
-    
+
     getHouse(id) {
         return this.getResource(`/houses/${id}/`);
     }
+    _transformCharacter(char) {
+        return {
+            name: char.name,
+            gender: char.gender,
+            born: char.born,
+            died: char.died,
+            culture: char.culture,
+        }
+    }
+    _transformHouse(){
+
+    }
+    
 }
