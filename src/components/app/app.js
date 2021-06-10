8a0de73403f7a6da0727e282a5ed3961f5db73a0
vadimcvirko <promiti90@gmail.com>
2021-06-10 15:34:09 +0300

    add  React router

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,14 +3,11 @@ import { Col, Row, Container } from "reactstrap";
 import Header from "../header";
 import RandomChar from "../randomChar";
 import ErrorMessage from "../error";
-import CharacterPage from "../characterPage";
-import ItemList from "../itemList";
-import CharDetails from "../charDetails";
 import gotService from "../../services/gotService";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import "./app.css";
-import { HousesPage, BooksPage } from "../pages";
+import { BooksPage,CharacterPage,HousesPage } from "../pages";
 
 export default class App extends Component {
   gotService = new gotService();
@@ -52,38 +49,10 @@ export default class App extends Component {
               </button>
               </Col>
             </Row>
-            {/* <CharacterPage /> */}
-
             <Route path='/characters'component={CharacterPage}/>
-            <Route path='/books'component={BooksPage}/>
-            <Route path='/housePage'component={HousesPage}/>
-
-{/* 
-            <Row>
-              <Col md="6">
-                <ItemList
-                  onItemSelected={this.onItemSelected}
-                  getData={this.gotService.getAllBooks}
-                  renderItem={(item) => `${item.name}`}
-                />
-              </Col>
-              <Col md="6">
-                <CharDetails charId={this.state.selectedChar} />
-              </Col>
-            </Row> */}
-
-            {/* <Row>
-              <Col md="6">
-                <ItemList
-                  onItemSelected={this.onItemSelected}
-                  getData={this.gotService.getAllHouses}
-                  renderItem={(item) => `${item.name}`}
-                />
-              </Col>
-              <Col md="6">
-                <CharDetails charId={this.state.selectedChar} />
-              </Col>
-            </Row> */}
+            <Route path='/housePage'  component={HousesPage}/>
+            <Route path='/books' exact component={BooksPage}/>
+            
           </Container>
         </div>
       </Router>
